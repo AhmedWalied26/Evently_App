682a74f09d26aef45d2d5a33fb1e3c3faee828de
Ahmed Walied <2301647@student.eelu.edu.eg>
2026-08-03 04:26:01 +0300

    feat(profile): add profile picture picker

--- lib/features/home/taps/profile_tap/profile_tap.dart
+++ lib/features/home/taps/profile_tap/profile_tap.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:evently_app/features/home/taps/profile_tap/widgets/custom_bottom_sheet.dart';
 import 'package:evently_app/features/home/taps/profile_tap/widgets/setting_tile.dart';
 import 'package:evently_app/features/home/taps/profile_tap/widgets/switch_mode.dart';
@@ -9,10 +10,29 @@ import 'package:evently_app/utils/app_routes.dart';
 import 'package:evently_app/utils/size_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
-class ProfileTap extends StatelessWidget {
+class ProfileTap extends StatefulWidget {
   const ProfileTap({super.key});
+
+  @override
+  State<ProfileTap> createState() => _ProfileTapState();
+}
+
+class _ProfileTapState extends State<ProfileTap> {
+  File? selectedImage;
+
+  final picker = ImagePicker();
+  Future<void> pickImage() async {
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      setState(() {
+        selectedImage = File(image.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +43,23 @@ class ProfileTap extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: height * 0.032),
-          CircleAvatar(
-            radius: 60,
-            backgroundImage: AssetImage(AppAssets.routeLogoImage),
+          InkWell(
+            overlayColor: WidgetStatePropertyAll(AppColors.transparentColor),
+            onTap: () {
+              pickImage();
+            },
+            child: CircleAvatar(
+              radius: 66,
+              backgroundColor: Theme.of(context).cardColor,
+              child: CircleAvatar(
+                radius: 62,
+                backgroundImage: selectedImage == null
+                    ? AssetImage(AppAssets.routeLogoImage)
+                    : FileImage(selectedImage!),
+              ),
+            ),
           ),
-          SizedBox(height: height * 0.02),
+          SizedBox(height: height * 0.01),
           Text(
             userProvider.userModel!.name,
             style: Theme.of(context).textTheme.headlineLarge,
